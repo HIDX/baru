4c995fd9bf02b7e70e95af1ee10d66e2c69b214f
HIDX <abduwahidkk123@gmail.com>
2019-08-13 22:35:54 +0700

    new

--- app/src/main/java/com/example/eventm/SplashScreenActivity.kt
+++ app/src/main/java/com/example/eventm/SplashScreenActivity.kt
@@ -15,7 +15,7 @@ class SplashScreenActivity : AppCompatActivity() {
             override fun run() {
                 try {
                     Thread.sleep(5000)
-                    val intent= Intent(baseContext, IntroActivity::class.java)
+                    val intent= Intent(baseContext, WelcomeActivity::class.java)
                     startActivity(intent)
                 }catch (e:Exception){
                     e.printStackTrace()
